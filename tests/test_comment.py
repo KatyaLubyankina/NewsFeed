3af4194cec174c4e6d9e114d9844ffc764c71f49
katyalubyankina <katylubyankina@gmail.com>
2023-06-25 18:16:03 +0300

    test_comment and test_post updated

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -6,7 +6,6 @@ from sqlalchemy.pool import StaticPool
 from src.db.database import Base
 from src.main import app
 from src.db.database import get_db
-import os
 
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test_api.db"
@@ -61,35 +60,6 @@ def login_user(test_db, create_user):
     return response
 
 
-def test_login(login_user):
-    assert login_user.status_code == 200
-    access_token = login_user.json().get("access_token")
-    assert access_token
-
-
-def test_upload_image_success(login_user):
-    access_token = login_user.json().get("access_token")
-    file_path = "tests/test.png"
-    if os.path.isfile(file_path):
-        file = {"image": open(file_path, "rb")}
-        response = client.post(
-            "/post/image",
-            files=file,
-            headers={
-                "Authorization": "bearer " + access_token
-            }
-        )
-        assert response.status_code == 200
-        assert response.json().get("filename")
-    else:
-        pytest.fail("File does not exists.")
-
-
-def test_get_all_posts(test_db):
-    response = client.get('/post/all')
-    assert response.status_code == 200
-
-
 @pytest.fixture()
 def create_post(login_user):
     access_token = login_user.json().get("access_token")
@@ -108,44 +78,36 @@ def create_post(login_user):
     return [response, access_token]
 
 
-def test_create_post(create_post):
-    response = create_post[0]
-    assert response.status_code == 200
-    assert response.json().get("caption") == "Test caption"
-
-
-def test_delete_post_success(create_post):
-    id = 1
+@pytest.fixture()
+def create_comment(create_post):
     access_token = create_post[1]
-    response = client.get(
-        f"/post/delete/{id}",
+    response = client.post(
+        "/comment",
+        json={
+            "username": "test",
+            "text": "Test comment",
+            "post_id": 1
+        },
         headers={
             "Authorization": "bearer " + access_token
-        }
-    )
+        })
+    return response
+
+
+def test_create_comment(create_comment):
+    response = create_comment
     assert response.status_code == 200
-    assert response.json() == "ok"
+    assert response.json().get('post_id') == 1
+    assert response.json().get('text') == 'Test comment'
+    assert response.json().get('id') == 1
+    assert response.json().get('username') == "test"
 
 
-def test_delete_post_token_error(create_post):
-    id = 1
-    access_token = ""
-    response = client.get(
-        f"/post/delete/{id}",
-        headers={
-            "Authorization": "bearer " + access_token
-        }
-    )
-    assert response.status_code == 401
-
-
-def test_delete_post_id_error(create_post):
-    id = 2
-    access_token = create_post[1]
-    response = client.get(
-        f"/post/delete/{id}",
-        headers={
-            "Authorization": "bearer " + access_token
-        }
-    )
-    assert response.status_code == 404
+def test_get_all_comments_success(create_comment):
+    post_id = 1
+    response = client.get(f'/comment/all/{post_id}')
+    assert response.status_code == 200
+    assert response.json()[0].get("id") == 1
+    assert response.json()[0].get("post_id") == 1
+    assert response.json()[0].get("text") == "Test comment"
+    assert response.json()[0].get("username") == 'test'
